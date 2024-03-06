4c6e9ac281138be7666ea0243b0d9f481ee2cce6
Zarifullah Niazi <zarifniazi@protonmail.com>
2024-03-07 00:20:18 +0430

    Fixed

--- countdown-app/src/App.tsx
+++ countdown-app/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="container">
       <div className="countdown">
         <h3>We are still cooking this project.</h3>
-        <p>We will launch hopefully, if we are not busy) in</p>
+        <p>We will launch hopefully, (if we are not busy) in</p>
         <div className="countdown-wrapper">
           <CountDown duration={4 * 24 * 60 * 60 * 1000} />
         </div>
